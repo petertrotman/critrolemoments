733251bf9b0178ed52aba234da13d2f3710457a2
= <petertrotman@gmail.com>
2017-02-22 20:22:57 +0000

    Youtube embeds working

--- src/moments.js
+++ src/moments.js
@@ -1,6 +1,7 @@
 import moments from '../data/moments.json';
 import styles from './styles.css';
 
+import { ytElement } from './youtube';
 import { parseTimestamp, padTimestamp } from './util';
 
 
@@ -17,22 +18,35 @@ function momentSortFn(a, b) {
   return seca > secb ? 1 : -1;
 }
 
-export function momentElement(moment) {
+export function momentElement(episode, moment) {
   const li = document.createElement('li');
   li.classList.add(styles.moment);
   li.innerHTML = `[${padTimestamp(moment.timestamp)}] ${moment.description}`;
+
+  const addYtPlayer = () => {
+    const width = li.getBoundingClientRect().width;
+    const yt = ytElement(episode, moment, width);
+    li.appendChild(yt);
+
+    li.onclick = () => {
+      yt.remove();
+      li.onclick = () => addYtPlayer();
+    };
+  };
+
+  li.onclick = () => addYtPlayer();
   return li;
 }
 
 
 export function momentsElement(episode) {
   const ul = document.createElement('ul');
-  ul.classList.add(styles.momentList);
+  ul.classList.add(styles.moments);
 
   Object.values(moments)
     .filter(m => m.episodeId === episode.id)
     .sort(momentSortFn)
-    .forEach(m => ul.appendChild(momentElement(m)));
+    .forEach(m => ul.appendChild(momentElement(episode, m)));
 
   return ul;
 }
---
+++ src/youtube.js
@@ -0,0 +1,25 @@
+import styles from './styles.css';
+import { parseTimestamp } from './util';
+
+export function ytElement(episode, moment, containerWidth) {
+  const iframe = document.createElement('iframe');
+  const ts = parseTimestamp(moment.timestamp);
+  const src =
+    `https://www.youtube.com/embed/${episode.videoId}` +
+    `?start=${(ts.h * 3600) + (ts.m * 60) + ts.s}` +
+    '&autoplay=1';
+
+  const padding = 5;
+  const width = containerWidth - (2 * padding);
+
+  iframe.classList.add(styles.ytPlayer);
+  iframe.setAttribute('type', 'text/html');
+  iframe.setAttribute('src', src);
+  iframe.setAttribute('width', `${width}px`);
+  iframe.setAttribute('height', `${width * (9 / 16)}px`);
+  iframe.setAttribute('frameborder', 0);
+  iframe.setAttribute('allowfullscreen', true);
+  iframe.setAttribute('style', `padding: ${padding}px;`);
+
+  return iframe;
+}
