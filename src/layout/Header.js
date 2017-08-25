798bbf77df70e49266b598cddf13fc2f21366bc1
= <petertrotman@gmail.com>
2017-08-25 11:10:51 +0100

    Basic header styling

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const StyledHeader = styled.header`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  height: 4em;
   width: 100%;
+
+  font-size: 2em;
+  padding-bottom: 0.2em;
+
+  > a {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
 
 const Header = () => (
   <StyledHeader>
-    Critical Role Moments
+    <Link to="/">
+      Header
+    </Link>
   </StyledHeader>
 );
 
