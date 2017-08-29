406ff56fa34d53210f7a0bbce71841024faae3de
= <petertrotman@gmail.com>
2017-08-29 12:44:47 +0100

    Settings now using redux store to get current user.

--- src/settings/Settings.js
+++ src/settings/Settings.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import firebase from 'firebase/app';
 
 import loginRequired from '../auth/loginRequired';
 
 class Settings extends React.Component {
-  componentWillMount() {
-    this.setState({ user: firebase.app().auth().currentUser });
+  propTypes = {
+    user: PropTypes.shape({
+      displayName: PropTypes.string.isRequired,
+    }).isRequired,
   }
 
   render() {
     return (
       <div>
-        <h1>{ this.state.user.displayName }</h1>
+        <h1>{ this.props.user.displayName }</h1>
         <Link to="/auth/signout">Sign Out</Link>
       </div>
     );
   }
 }
 
-export default loginRequired(Settings);
+export default compose(
+  connect(store => ({ user: store.auth.user })),
+  loginRequired,
+)(Settings);
