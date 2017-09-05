1e83325d1afdf7876500d4bcf263da4da5a4cf18
= <petertrotman@gmail.com>
2017-09-05 15:30:52 +0100

    Cleaned up to use only one interation of Object.entries

--- functions/indexes.js
+++ functions/indexes.js
@@ -2,41 +2,35 @@ function indexMoments(db) {
   db.ref('/moments').once('value')
     .then(snapshot => snapshot.val())
     .then((moments) => {
-      const byEpisode =
-        Object.keys(moments)
-          .map(key => [key, moments[key]])
-          .reduce((acc, [key, moment]) => {
-            // return Object.assign(
-            //   {},
-            //   acc,
-            //   { [moment.episode]: Object.assign(
-            //     {},
-            //     acc[moment.episode],
-            //     { [key]: true },
-            //   ) },
-            // );
-            if (!acc[moment.episode]) acc[moment.episode] = {};
-            acc[moment.episode][key] = true;
-            return acc;
-          }, {});
+      const momentsEntries = Object.keys(moments).map(key => [key, moments[key]]);
 
-      const byTimestamp =
-        Object.keys(moments)
-          .map(key => [key, moments[key]])
-          .sort((a, b) => a[1].timestamp - b[1].timestamp)
-          .map(entry => entry[0]);
+      const byEpisode = momentsEntries
+        .reduce((acc, [key, moment]) => {
+          // return Object.assign(
+          //   {},
+          //   acc,
+          //   { [moment.episode]: Object.assign(
+          //     {},
+          //     acc[moment.episode],
+          //     { [key]: true },
+          //   ) },
+          // );
+          if (!acc[moment.episode]) acc[moment.episode] = {};
+          acc[moment.episode][key] = true;
+          return acc;
+        }, {});
 
-      const byStarCount =
-        Object.keys(moments)
-          .map(key => [key, moments[key]])
-          .sort((a, b) => a[1].starCount - b[1].starCount)
-          .map(entry => entry[0]);
+      const byTimestamp = momentsEntries
+        .sort((a, b) => a[1].timestamp - b[1].timestamp)
+        .map(entry => entry[0]);
 
-      const byStart =
-        Object.keys(moments)
-          .map(key => [key, moments[key]])
-          .sort((a, b) => a[1].start - b[1].start)
-          .map(entry => entry[0]);
+      const byStarCount = momentsEntries
+        .sort((a, b) => a[1].starCount - b[1].starCount)
+        .map(entry => entry[0]);
+
+      const byStart = momentsEntries
+        .sort((a, b) => a[1].start - b[1].start)
+        .map(entry => entry[0]);
 
       db.ref('/index/moments')
         .set({ byEpisode, byTimestamp, byStarCount, byStart });
