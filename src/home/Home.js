2a721e76c84ec70cf5da8c1a4f78e0763a9e995f
= <petertrotman@gmail.com>
2017-09-15 23:42:24 +0100

    Firebase database rules

--- src/home/Home.js
+++ src/home/Home.js
@@ -16,6 +16,13 @@ const Container = styled.div`
     font-size: 1.2em;
     font-weight: 600;
   }
+
+  a {
+    color: ${props => props.theme.secondary};
+    :visited {
+      color: ${props => props.theme.secondary};
+    }
+  }
 `;
 
 const Home = () => (
@@ -26,6 +33,7 @@ const Home = () => (
       <ul>
         <li>Click <Link to="/explore">Explore</Link> in the navigation bar (the aperture icon).</li>
         <li>Have a browse through the moments using the available filters.</li>
+        <li>Watch the moments by clicking on the title text.</li>
         <li>Click the Heart next to the ones you particularly like.</li>
         <li>Share moments with your friends with the Share button.</li>
         <li>Review your saved moments in the <Link to="/moments">Moments</Link> tab (the heart icon).</li>
@@ -34,13 +42,20 @@ const Home = () => (
       <ul>
         <li>Create new moments for the community in the <Link to="/create">Create</Link> tab (the plus icon).</li>
         <li>Copy and Claim moments by Editing them as you explore.</li>
-        <li>Send Feedback using the link in the <Link to="/settings">Settings</Link> tab (the cog icon).</li>
+        <li>Send Feedback and Bug Reports using the link in the <Link to="/settings">Settings</Link> tab (the cog icon).</li>
+        <li>Report moments that aren't suitable using the Report button.</li>
       </ul>
       <p>More Advanced guide:</p>
       <ul>
-        <li>Come to the repository on <a href="https://github.com/petertrotman/critrolemoments">Github</a> and contribute your skills!</li>
+        <li>
+          Come to the repository on <a href="https://github.com/petertrotman/critrolemoments">Github</a> and contribute your skills!
+          <ul>
+            <li>Seriously we could really use a good designer and a good content writer...</li>
+          </ul>
+        </li>
       </ul>
-      <p>I hope you find this tool useful and I can't wait to see all the amazing moments that I've missed so far!</p>
+      <p>Please note that this website is by no means finished, and I need your feedback to turn it into the amazing place I know it can be.</p>
+      <p>I hope you find this tool useful and I can't wait to see all the great CritRole Moments that I've missed so far!</p>
     </main>
   </Container>
 );
